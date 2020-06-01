e138b233cb59661aeeff39eb37034850c30e7a6c
Kane Chan <kanes.chan@gmail.com>
2020-06-01 11:26:20 -0300

    Implementado a visualização dos registros no modelo de "aplicação" do usuário. Pode acompanhar a implementação através do vídeo: https://youtu.be/_jQW9UvNC2g

--- crudemjava/src/br/com/agenda/aplicacao/Main.java
+++ crudemjava/src/br/com/agenda/aplicacao/Main.java
@@ -19,11 +19,17 @@ public class Main {
 		ContatoDAO contatoDao = new ContatoDAO();
 
 		Contato contato = new Contato();
-		contato.setNome("João Gabriel");
-		contato.setIdade(78);
+		contato.setNome("Maria Gabriela Dias");
+		contato.setIdade(35);
 		contato.setDataCadastro(new Date());
 		
 		contatoDao.save(contato);
+		
+		//Visualização dos registros do banco de dados TODOS
+		
+		for(Contato c : contatoDao.getContatos()) {
+			System.out.println("Contato: "+c.getNome());
+		}
 	}
 
 }
